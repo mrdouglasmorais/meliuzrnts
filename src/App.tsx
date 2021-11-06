da61a76b53bcdc7cd584c2eaa0f706097638b5cd
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2021-11-06 12:19:15 -0300

    refe stack navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
 import Home from './Screens/Home';
 
 const App: React.FC = () => {
-  return <Home />;
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
